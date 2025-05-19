7a42fc700ebd117127b71dd17b3d830d13afd1f0
Artifice120 <160672410+Artifice120@users.noreply.github.com>
2025-05-19 15:46:41 -0400

    Create init.bash

---
+++ init.bash
@@ -0,0 +1,17 @@
+#!bin/bash/
+
+__conda_setup="$('/opt/conda/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/opt/conda/etc/profile.d/conda.sh" ]; then
+        . "/opt/conda/etc/profile.d/conda.sh"
+    else
+        export PATH="/opt/conda/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+
+export PATH="/opt/conda/bin:$PATH"
+
+conda activate /opt/conda/envs/edta
